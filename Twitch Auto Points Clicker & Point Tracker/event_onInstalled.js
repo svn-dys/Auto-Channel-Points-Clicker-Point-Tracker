354984466a41f7d04ddcd25f6cce1033e1551c0b
svn-dys <11863033+svn-dys@users.noreply.github.com>
2021-05-25 01:49:43 -0400

    Git issue delete directory

--- Twitch Auto Points Clicker & Point Tracker/event_onInstalled.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(function (details) {
-  if (details.reason == "install") {
-    var pnts_start = 0;
-    prompt("Please enter your name", "Harry Potter");
-    console.log("Thank you for installing my extension!!! PogChamp");
-    chrome.storage.sync.set({ points: pnts_start }, function () {});
-    localStorage.setItem("install", 1, function () {});
-  }
-});
