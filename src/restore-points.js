66950c308920f6e6a7eef3d27d83f40a1b58f507
svn-dys <11863033+svn-dys@users.noreply.github.com>
2023-03-18 16:23:48 -0400

    change UNFORMAtTED_POINTS to UNFORMATTED_POINTS
    const capitalization fix

--- src/restore-points.js
+++ src/restore-points.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", () =>  {
         const NUM_POINTS_ELEMENT = document.getElementsByClassName("num-points-text")[0];
         const POINTS_GRABBED_STRING = "Points grabbed by extension:";
         const FORMATTED_POINTS = kFormatter(result.points);
-        const UNFORMAtTED_POINTS = result.points;
+        const UNFORMATTED_POINTS = result.points;
         let bottom_text = POINTS_GRABBED_STRING + " " + FORMATTED_POINTS;
         
         NUM_POINTS_ELEMENT.innerHTML = bottom_text;
         NUM_POINTS_ELEMENT.title = `Total points grabbed by this extension at twitch.tv
         
-Total Points: ${UNFORMAtTED_POINTS}\n\n`
+Total Points: ${UNFORMATTED_POINTS}\n\n`
         
-        console.log("Points restored:", UNFORMAtTED_POINTS);
+        console.log("Points restored:", UNFORMATTED_POINTS);
     });
 });
